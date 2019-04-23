22301320fc6761b4c831b15b9d6889859cceadd4
53554 <53554@etu.he2b.be>
2019-04-23 11:10:50 +0200

    Comit 03

--- src/g53554/FourInLine/Column.java
+++ src/g53554/FourInLine/Column.java
@@ -9,14 +9,48 @@ import java.util.List;
 public class Column {
 
     private List<Disc> discs;
+    private int HEIGHT = 6;
 
     /**
-     * The constructor of the class column 
+     * The constructor of the class column
+     *
      * @param discs
      */
-
     public Column(List<Disc> discs) {
         this.discs = discs;
+
+    }
+
+    /**
+     * This method return a boolean if the list is full
+     *
+     * @return
+     */
+    public boolean isFull() {
+        boolean full = false;
+        for (int i = 0; i < this.discs.size(); i++) {
+            if (i == this.HEIGHT) {
+                full = true;
+            }
+
+        }
+
+        return full;
+    }
+
+    /**
+     * This method add a disk to the column
+     *
+     * @param disc
+     */
+    public void drop(Disc disc) {
+        Column column = new Column(discs);
+        if (column.isFull()) {
+            throw new IllegalStateException("La colonne est remplie");
+        } else {
+            column.discs.add(disc);
+        }
+
     }
 
 }
