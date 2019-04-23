a332b779011704c6bc137e4fd085d5a693f62f11
53554 <53554@etu.he2b.be>
2019-04-23 10:56:54 +0200

    Comit 03

---
+++ src/g53554/FourInLine/Column.java
@@ -0,0 +1,22 @@
+package g53554.FourInLine;
+
+import java.util.List;
+
+/**
+ *
+ * @author jussy
+ */
+public class Column {
+
+    private List<Disc> discs;
+
+    /**
+     * The constructor of the class column 
+     * @param discs
+     */
+
+    public Column(List<Disc> discs) {
+        this.discs = discs;
+    }
+
+}
